9629895938729200c0e0996b54d0ca27a6496f23
Tin Tran <tin.tran0519@gmail.com>
2018-12-08 20:12:41 -0800

    added reminderForm component, container, and stylesheet

---
+++ src/components/forms/ReminderForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../../stylesheets/forms/ReminderForm.scss';
+
+class ReminderForm extends React.Component {
+  render () {
+    return (
+      <div className="reminder-form">
+
+        <div className="header">
+          <h2>Welcome to Honeybee Health</h2>
+          <h4>Sign-up for reminder emails!</h4>
+        </div>
+
+      </div>
+     );
+  };
+}
+
+export default ReminderForm;
---
+++ src/containers/forms/ReminderFormContainer.js
@@ -0,0 +1,12 @@
+import React        from 'react';
+import ReminderForm from '../../components/forms/ReminderForm';
+
+class ReminderFormContainer extends React.Component {
+  render () {
+    return (
+      <ReminderForm />
+    )
+  }
+}
+
+export default ReminderFormContainer;
